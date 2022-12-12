f770d4bd3fb50ffc30439e2c13191f2d05d6e0f3
Fufek-git <arek.jkirejczyk@gmail.com>
2022-12-12 18:06:52 +0100

    Working

--- GPT-125M-UI.py
+++ GPT-125M-UI.py
@@ -2,6 +2,7 @@ import tkinter
 from transformers import pipeline
 import time
 import linecache
+from ttkthemes import ThemedStyle
 
 generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
 
@@ -11,9 +12,9 @@ generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
 master=tkinter.Tk()
 master.title("GPT-Neo Ui")
 master.geometry("1920x1080")
-# Function for getting Input
-# from textbox and printing it 
-# at label widget
+style = ThemedStyle(master)
+master.tk.call("source", "forest-dark.tcl")
+style.theme_use("forest-dark")
   
 def printInput():
     inp = inputtxt.get(1.0, "end-1c")
@@ -47,16 +48,16 @@ inputtxt.pack()
   
 # Button Creation
 printButton = tkinter.Button(master, text = "Generate", height=4, width=20, command = printInput, bg='orange', activebackground="green")
-printButton.place(x=860, y=930)
+printButton.place(x=860, y=860)
 
 clearButton = tkinter.Button(master,text = "Clear", height=4, width=15, command = clearInput, bg='dimgray', activebackground="red")
-clearButton.place(x=700, y=930)
+clearButton.place(x=700, y=860)
 
 copyButton = tkinter.Button(master,text = "Copy", height=2, width=15, command = save_text, bg='dimgray', activebackground="green")
-copyButton.place(x=1060, y=925)
+copyButton.place(x=1060, y=850)
 
 pasteButton = tkinter.Button(master,text = "Paste", height=2, width=15, command = open_text, bg='dimgray', activebackground="red")
-pasteButton.place(x=1060, y=970)
+pasteButton.place(x=1060, y=905)
 
 
   
