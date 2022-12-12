60ea081e5c63ce8db6110d06c06614bddb8598aa
Arek J-K <arek.jkirejczyk@gmail.com>
2022-12-12 17:31:35 +0100

    Theme update

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 import os
-os.system("pip install tk")
+os.system("sudo apt-get install python3-tk")
+os.system("sudo dnf install python3-tkinter")
 os.system("pip install transformers")
+os.system("pip install ttkthemes")
